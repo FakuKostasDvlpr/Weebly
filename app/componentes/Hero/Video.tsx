16893922672c0b301429e7cbebb877db06eccacf
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-14 17:50:16 -0300

    video

--- app/componentes/Hero/Video.tsx
+++ app/componentes/Hero/Video.tsx
@@ -3,7 +3,7 @@ export function PlayVideo() {
    
     return (
         <div className="flex mx-auto" id="videoPlay">
-            <iframe src="https://player.vimeo.com/video/898790021?h=b02714ffe4" width="480" height="480" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+            <iframe src="https://player.vimeo.com/video/898790021?h=b02714ffe4" width="460" height="460" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 }
