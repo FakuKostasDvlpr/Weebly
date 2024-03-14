2b8090a22db3b22581927528dd6beabd9e72b881
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-14 20:12:05 -0300

    paage/video

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { GlobeDemo } from "./componentes/Hero/Portada";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-transparent w-full max-w-[100px] justify-center items-center text-center m-auto">
+    <main className="flex min-h-screen flex-col bg-transparent w-full max-w-[900px] justify-center items-center text-center m-auto">
       <GlobeDemo/>
     </main>
   );
