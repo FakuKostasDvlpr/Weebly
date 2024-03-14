8eac478c3d35f188bfce8c685892cc81a84bdfd9
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-14 17:51:08 -0300

    page css

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { PlayVideo } from "./componentes/Hero/Video";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-transparent w-full  max-w-[1200px] mx-auto">
+    <main className="flex min-h-screen flex-col bg-transparent w-full max-w-[1100px] justify-center items-center text-center m-auto">
       <GlobeDemo/>
       <Presentacion/>
     </main>
